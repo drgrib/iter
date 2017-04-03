1116712fe4d876977465cc6b25cf6fcb591e27a6
Chris Redford <credford@gmail.com>
2017-04-03 14:18:01 -0700

    also not great

--- iter_test.go
+++ iter_test.go
@@ -1,28 +1,87 @@
+/*
+	Run using
+	go test -bench=. -run=.
+*/
+
 package iter
 
 import (
+	bIter "github.com/bradfitz/iter"
 	"testing"
 )
 
-// const loops = 1e6
-const loops = 10
+//////////////////////////////////////////////
+/// many loops
+//////////////////////////////////////////////
 
-func BenchmarkForClause(b *testing.B) {
+const loopsMany = 1e6
+
+func BenchmarkForMany(b *testing.B) {
 	b.ReportAllocs()
 	j := 0
 	for i := 0; i < b.N; i++ {
-		for j = 0; j < loops; j++ {
+		for j = 0; j < loopsMany; j++ {
 			j = j
 		}
 	}
 	_ = j
 }
 
-func BenchmarkRangeIter(b *testing.B) {
+func BenchmarkDrgribIterMany(b *testing.B) {
 	b.ReportAllocs()
 	j := 0
 	for i := 0; i < b.N; i++ {
-		for j = range N(loops) {
+		for j = range N(loopsMany) {
+			j = j
+		}
+	}
+	_ = j
+}
+
+func BenchmarkBradfitzIterMany(b *testing.B) {
+	b.ReportAllocs()
+	j := 0
+	for i := 0; i < b.N; i++ {
+		for j = range bIter.N(loopsMany) {
+			j = j
+		}
+	}
+	_ = j
+}
+
+//////////////////////////////////////////////
+/// loops10
+//////////////////////////////////////////////
+
+const loops10 = 10
+
+func BenchmarkFor10(b *testing.B) {
+	b.ReportAllocs()
+	j := 0
+	for i := 0; i < b.N; i++ {
+		for j = 0; j < loops10; j++ {
+			j = j
+		}
+	}
+	_ = j
+}
+
+func BenchmarkDrgribIter10(b *testing.B) {
+	b.ReportAllocs()
+	j := 0
+	for i := 0; i < b.N; i++ {
+		for j = range N(loops10) {
+			j = j
+		}
+	}
+	_ = j
+}
+
+func BenchmarkBradfitzIter10(b *testing.B) {
+	b.ReportAllocs()
+	j := 0
+	for i := 0; i < b.N; i++ {
+		for j = range bIter.N(loops10) {
 			j = j
 		}
 	}

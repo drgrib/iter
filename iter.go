98c3a411036c113a883b94e157ce5fe750fa7b17
Chris Redford <credford@gmail.com>
2017-04-03 13:55:25 -0700

    doc

--- iter.go
+++ iter.go
@@ -1,5 +1,27 @@
+// Package iter provides a maximally efficient and concise range over integers [0,n) backed internally by a simple for loop
 package iter
 
+/*
+N returns a `chan int` that will return [0,n)
+For example:
+
+	for i := range iter.N(10) {
+		fmt.Println(i)
+	}
+
+will print
+
+	0
+	1
+	2
+	3
+	4
+	5
+	6
+	7
+	8
+	9
+*/
 func N(n int) chan int {
 	c := make(chan int)
 	go func() {
